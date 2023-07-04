a035cb59173c66898a64b4dd4099b9e0b2129c04
Phạm Minh Tấn <85481444+PhamMinhTan1122@users.noreply.github.com>
2023-07-04 16:34:31 +0700

    Lab (#2)
    * lab
    
    * run success without tailwind css
    
    * run success without tailwind css
    
    * Finall app

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import sveltePreprocess from 'svelte-preprocess';
 
 export default {
-  preprocess: sveltePreprocess()
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: sveltePreprocess()
 };
