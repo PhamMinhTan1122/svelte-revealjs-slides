c09ac15732043388c64ff1670c014b55682116d9
Phạm Minh Tấn <85481444+PhamMinhTan1122@users.noreply.github.com>
2023-07-04 14:56:44 +0700

    Lab (#1)
    * lab
    
    * run success without tailwind css
    
    * run success without tailwind css

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 import Highlight from 'reveal.js/plugin/highlight/highlight';
+import Markdown from 'reveal.js/plugin/markdown/markdown';
+import MathReveal from 'reveal.js/plugin/math/math';
+import RevealNotes from 'reveal.js/plugin/notes/notes';
 
 // Import theme
 import 'reveal.js/dist/theme/black.css';
@@ -9,11 +12,26 @@ import 'reveal.js/plugin/highlight/monokai.css';
 export default {
     // App Config
     app: {
-        name: 'Minh Tan',
+        name: 'Reveal.js with Svelte and Vite',
     },
     // Reveal Config
     reveal: {
-        plugins: [Highlight],
+        plugins: [ Highlight, Markdown, MathReveal.MathJax2, MathReveal.KaTeX, RevealNotes ],
         hash: true,
+        autoAnimateEasing: 'ease',
+        autoAnimateDuration: 1,
+        controls: true,
+        progress: true,
+        touch: true,
+        transition: 'convex',
+        mathjax2: {
+            config: 'TeX-AMS_HTML-full',
+            TeX: {
+                Macros: {
+                    R: '\\mathbb{R}',
+                    set: [ '\\left\\{#1 \\; ; \\; #2\\right\\}', 2 ]
+                }
+            }
+        },
     },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import App    from './App.svelte';
 import config from './config';
+import './app.css'
 
 const app = new App({
     target: document.body,
